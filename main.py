c12b5d45a02bc62bad62eb2d4ea84f504bdd4094
Ken760 <49761163+Ken760@users.noreply.github.com>
2023-04-29 22:57:17 +0300

    test

--- main.py
+++ main.py
@@ -1,26 +1,73 @@
-from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt6.QtSql import *
+# from PyQt6 import uic
+# from PyQt6.QtCore import Qt
+# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+# from PyQt6.QtSql import *
+# import requests
+# from PyQt6.uic.properties import QtWidgets
+#
+# Form, Window = uic.loadUiType("MainForm.ui")
+#
+#
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setWindowTitle("My App")
+#         button = QPushButton("Press Me!")
+#
+#         # Set the central widget of the Window.
+#         self.setCentralWidget(button)
+#
+#     def getData(self):
+#         model = self.listview('a')
+#         return 'hello'
+#
+#
+#
+#
+#
+# app = QApplication([])
+# window = Window()
+# form = Form()
+# form.setupUi(window)
+# window.show()
+# app.exec()
+
 import requests
-from PyQt6.uic.properties import QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QListView, QPushButton, QMessageBox
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
 
-Form, Window = uic.loadUiType("MainForm.ui")
-
-class MainWindow(QMainWindow):
+class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+        self.model = QStandardItemModel()
+        self.listview = QListView()
+        self.listview.setModel(self.model)
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.btn_get = QPushButton('Get Data')
+        self.btn_get.clicked.connect(self.get_data)
 
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.listview)
+        vbox.addWidget(self.btn_get)
 
+        self.setLayout(vbox)
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec()
+    def get_data(self):
+        url = 'http://localhost:5000/get_data'
+        response = requests.get(url)
+        data = response.json()
+
+        self.model.clear()
+        for item in data:
+            text = f'{item["id"]} - {item["text"]} - {item["date"]} - {item["time"]}'
+            qitem = QStandardItem(text)
+            self.model.appendRow(qitem)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    w = MyWidget()
+    w.show()
+    app.exec_()
